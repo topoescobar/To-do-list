e5b09f17eb4ed71a248ec9d9279a085403e1e181
topoescobar <ivan90ge@gmail.com>
2023-07-16 23:18:44 -0300

    URL

--- src/services/getAPI.js
+++ src/services/getAPI.js
@@ -1,8 +1,11 @@
 import axios from 'axios'
 
+// const URL =  'http://localhost:3001/api/tasks/'
+const URL =  'https://extinct-teal-swordfish.cyclic.app/api/tasks/'
+
 export const getAPI = async() => {
   return axios
-    .get('http://localhost:3001/api/tasks/')
+    .get(URL)
     .then((response => {
       console.log('responseAPI', response.data)
       return response.data
--- src/services/postAPI.js
+++ src/services/postAPI.js
@@ -1,8 +1,11 @@
 import axios from 'axios'
 
+// const URL =  'http://localhost:3001/api/tasks/'
+const URL =  'https://extinct-teal-swordfish.cyclic.app/api/tasks/'
+
 export const postAPI = async (object) =>{
   return axios
-    .post('http://localhost:3001/api/tasks/', object)
+    .post(URL, object)
     .then((response) => {
       return response.data;
     })
